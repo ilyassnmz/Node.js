740f3c05f2bb4885e617c33b4bdbe54cd574bf57
1lyass1 <lunap.ilyas@gmail.com>
2025-08-07 17:16:53 +0300

    Deneme

---
+++ ExpresJS/routes/admin.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+const path = require("path");
+
+router.use("/blog/create", function(req, res) {
+    res.render( "admin/blog-create");
+});
+
+router.use("/blogs/:blogid", function(req, res) {
+   res.render( "admin/blog-edit");
+});
+
+router.use("/blogs", function(req, res) {
+    res.render( "admin/blog-list");
+});
+
+
+module.exports = router;
