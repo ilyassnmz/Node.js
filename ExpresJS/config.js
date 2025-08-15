e365044c25ba8d5eedc874386c722cdd7fe88a15
Ilyas Sonmez <lunap.ilyas@gmail.com>
2025-08-15 23:22:58 +0300

    Blog Details

---
+++ ExpresJS/config.js
@@ -0,0 +1,10 @@
+const config = {
+    db:{
+        host: "localhost",
+        user: "root",
+        password: "1304dxgroup",
+        database: "blogapp"
+    }
+}
+
+module.exports = config;
--- ExpresJS/routes/user.js
+++ ExpresJS/routes/user.js
@@ -1,55 +1,56 @@
 const express = require("express");
 const router = express.Router();
 
-const data = {
-    title: "Popüler Kurslar",
-    categories: ["Web Geliştirme", "Programlama", "Mobil Uygulamalar", "Veri Analizi", "Ofis Uygulamaları"],
-    blogs: [
-        {
-            blogid: 1,
-            baslik: "Komple Uygulamalı Web Geliştirme",
-            aciklama: "Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc",
-            resim: "1.jpeg",
-            anasayfa: true,
-            onay: true
-        },
-        {
-            blogid: 2,
-            baslik: "Python ile Sıfırdan İleri Seviye Python Programlama",
-            aciklama: "Sıfırdan İleri Seviye Python Dersleri.Veritabanı,Veri Analizi,Bot Yazımı,Web Geliştirme(Django)",
-            resim: "2.jpeg",
-            anasayfa: true,
-            onay: false
-        },
-        {
-            blogid: 3,
-            baslik: "Sıfırdan İleri Seviye Modern Javascript Dersleri ES7+",
-            aciklama: "Modern javascript dersleri ile (ES6 & ES7+) Nodejs, Angular, React ve VueJs için sağlam bir temel oluşturun.",
-            resim: "3.jpeg",
-            anasayfa: false,
-            onay: true
-        },
-        {
-            blogid: 3,
-            baslik: "Sıfırdan İleri Seviye Modern Javascript Dersleri ES7+",
-            aciklama: "Modern javascript dersleri ile (ES6 & ES7+) Nodejs, Angular, React ve VueJs için sağlam bir temel oluşturun.",
-            resim: "3.jpeg",
-            anasayfa: false,
-            onay: true
-        },
-    ]
-}
+const db = require("../data/db");
 
-router.use("/blogs/:blogid", function(req, res) {
-    res.render("users/blog-details");
+router.use("/blogs/:blogid", async function(req, res) {
+    const id = req.params.blogid;
+    try {
+        const [blog, ] = await db.execute("select * from blog where blogid=?", [id]);
+
+        res.render("users/blog-details",{
+            title: blog[0].baslik,
+            blog: blog[0]
+        });
+    }
+    catch(err){
+        console.log(err);
+    }
 });
 
-router.use("/blogs", function(req, res) {
-    res.render("users/blogs", data);
+router.use("/blogs", async function(req, res) {
+
+    try{
+        const [blogs, ] = await db.execute("select * from blog where onay=1")
+        const [categories, ] = await db.execute("select * from category");
+
+        res.render("users/blogs", {
+            title: "Tüm Kurslar",
+            blogs: blogs,
+            categories: categories
+        })
+    }
+    catch(err){
+        console.log(err);
+    }
+
 });
 
-router.use("/", function(req, res) {
-    res.render("users/index", data);
+router.use("/", async function(req, res) {
+
+    try{
+        const [blogs, ] = await db.execute("select * from blog where onay=1 and anasayfa=1")
+        const [categories, ] = await db.execute("select * from category");
+
+        res.render("users/index", {
+            title: "Popüler Kurslar",
+            blogs: blogs,
+            categories: categories
+        })
+    }
+    catch(err){
+        console.log(err);
+    }
 });
 
 module.exports = router;
