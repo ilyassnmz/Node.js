7414ba746719392c4d425fb3f7521502b135c6ff
Ilyas Sonmez <lunap.ilyas@gmail.com>
2025-08-16 22:32:49 +0300

    Kategori-Blog İlişkilendirilmesi

--- ExpresJS/routes/user.js
+++ ExpresJS/routes/user.js
@@ -3,15 +3,38 @@ const router = express.Router();
 
 const db = require("../data/db");
 
+router.use("/blogs/category/:categoryid", async function(req, res){
+    const id = req.params.categoryid;
+    try{
+        const [blogs, ] = await db.execute("select * from blog where categoryid=?", [id]);
+        const [categories, ] = await db.execute("select * from category");
+
+        res.render("users/blogs", {
+            title: "Tüm Kurslar",
+            blogs: blogs,
+            categories: categories
+        })
+    }
+    catch(err) {
+        console.log(err);
+    }
+});
+
 router.use("/blogs/:blogid", async function(req, res) {
     const id = req.params.blogid;
     try {
-        const [blog, ] = await db.execute("select * from blog where blogid=?", [id]);
+        const [blogs, ] = await db.execute("select * from blog where blogid=?", [id]);
 
-        res.render("users/blog-details",{
-            title: blog[0].baslik,
-            blog: blog[0]
-        });
+        const blog = blogs[0];
+
+        if(blog) {
+            return res.render("users/blog-details",{
+                title: blogs[0].baslik,
+                blog: blogs[0]
+            });
+
+        }
+        res.redirect("/");
     }
     catch(err){
         console.log(err);
