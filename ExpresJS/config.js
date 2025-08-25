d37796ca6af6287171bf488260a54f0ad4ac811c
Ilyas Sonmez <lunap.ilyas@gmail.com>
2025-08-25 23:13:55 +0300

    Sequelize ile db bağlantısı yapıldı

--- ExpresJS/config.js
+++ ExpresJS/config.js
@@ -3,7 +3,7 @@ const config = {
         host: "localhost",
         user: "root",
         password: "1304dxgroup",
-        database: "blogapp"
+        database: "blogdb"
     }
 }
 
--- ExpresJS/data/db.js
+++ ExpresJS/data/db.js
@@ -1,18 +1,39 @@
 const mysql = require("mysql2");
 const config = require("../config");
 
-let connection = mysql.createConnection(config.db);
+const Sequelize = require("sequelize");
 
-connection.connect(function(err){
-    if(err) {
-        return console.log(err);
-    }
-
-    connection.query("select * from blog", function(err, result){
-        console.log(result[0].baslik);
-    })
-
-    console.log("MySQL Server Bağlantısı Yapıldı")
+const sequelize = new Sequelize(config.db.database, config.db.user, config.db.password, {
+    dialect: "mysql",
+    host: config.db.host,
 });
 
-module.exports = connection.promise();
+async function connect() {
+    try {
+        sequelize.authenticate();
+        console.log("MySQL Server Bağlantısı Yapıldı");
+    }
+    catch (err) {
+        console.log("MySQL Bağlantı Hatası: ", err);
+    }
+}
+
+connect();
+
+module.exports = sequelize;
+
+// let connection = mysql.createConnection(config.db);
+
+// connection.connect(function(err){
+//     if(err) {
+//         return console.log(err);
+//     }
+
+//     connection.query("select * from blog", function(err, result){
+//         console.log(result[0].baslik);
+//     })
+
+//     console.log("MySQL Server Bağlantısı Yapıldı")
+// });
+
+// module.exports = connection.promise();
