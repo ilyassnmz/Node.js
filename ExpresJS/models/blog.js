da4ef7a33c4251da4078942de8fa423386e0fe1b
Ilyas Sonmez <lunap.ilyas@gmail.com>
2025-08-26 22:31:01 +0300

    Sequelize ile veri ekleme

--- ExpresJS/models/blog.js
+++ ExpresJS/models/blog.js
@@ -39,8 +39,32 @@ const Blog = sequelize.define("blog", {
 });
 
 async function sync() {
-    await Blog.sync({ force: true});
+    await Blog.sync({ alter: true });
     console.log("Blog tablosu oluşturuldu!");
+    
+    const count = await Blog.count();
+    if (count == 0) {
+        
+        await Blog.create({
+            baslik: "Komple Uygulamalı Web Geliştirme Eğitimi",
+            altbaslik: " Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc",
+            aciklama: "Web geliştirme komple bir web sitesinin hem web tasarım (html,css,javascript), hem de web programlama (asp.net mvc) konularının kullanılarak geliştirilmesidir. Sadece html css kullanarak statik bir site tasarlayabiliriz ancak işin içine bir web programlama dilini de katarsak dinamik bir web uygulaması geliştirmiş oluruz. ",
+            resim: "1.jpeg",
+            anasayfa: true,
+            onay: true,
+            categoryid: 1
+        });
+        await Blog.create({
+            baslik: "Python ile Sıfırdan İleri Seviye Python Programlama",
+            altbaslik: "Sıfırdan İleri Seviye Python Dersleri.Veritabanı,Veri Analizi,Bot Yazımı,Web Geliştirme(Django)",
+            aciklama: "Python, son zamanların en popüler programlama dili haline geldi. Python' ın bu kadar popüler olmasındaki sebep şüphesiz öğrenmesi kolay bir yazılım dili olmasıdır.sadikturan adreslerinde paylaşmış olduğum python dersleri serisini takip ederek ister video ister yazılı kaynaklar yardımıyla kısa zamanda python programlama alanında uzmanlık kazanın ve hayal ettiğiniz projeyi gerçekleştirin.",
+            resim: "2.jpeg",
+            anasayfa: true,
+            onay: true,
+            categoryid: 1
+        });
+    }
+
 }
 
 sync();
