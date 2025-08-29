b0b81775f27f603e917ac9505ce0397e7638e29b
Ilyas Sonmez <lunap.ilyas@gmail.com>
2025-08-29 23:02:23 +0300

    One to Many ilişkisinin uygulanması

--- ExpresJS/models/category.js
+++ ExpresJS/models/category.js
@@ -2,12 +2,6 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../data/db");
 
 const Category = sequelize.define("category", {
-    categoryid: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
     name: {
         type: DataTypes.STRING,
         allowNull: false
@@ -15,28 +9,5 @@ const Category = sequelize.define("category", {
 }, {
     timestamps: false
 });
-
-async function sync() {
-    await Category.sync({ alter: true });
-    console.log("category tablosu eklendi");
-
-    // await Category.create({ name: "Web Geliştirme" });
-    // await Category.create({ name: "Mobil Geliştirme" });
-    // await Category.create({ name: "Programlama" });
-
-    const count = await Category.count();
-
-    if(count == 0) { 
-        await Category.bulkCreate([
-            { name: "Web Geliştirme" },
-            { name: "Mobil Geliştirme" },
-            { name: "Programlama" }
-        ])
-    }
-
-    console.log("kategoriler eklendi");
-}
-
-sync();
 
 module.exports = Category;
