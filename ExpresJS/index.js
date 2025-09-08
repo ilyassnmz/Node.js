817742a0d9d0e4abcf63489a9f62b0a7f616c4d1
Ilyas Sonmez <lunap.ilyas@gmail.com>
2025-09-08 23:24:10 +0300

    Login-Register page

--- ExpresJS/index.js
+++ ExpresJS/index.js
@@ -8,11 +8,13 @@ app.use(express.urlencoded({ extended: false }));
 const path = require("path");
 const userRoutes = require("./routes/user");
 const adminRoutes = require("./routes/admin");
+const authRoutes = require("./routes/auth");
 
 app.use("/libs", express.static(path.join(__dirname, "node_modules")));
 app.use("/static", express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
+app.use("/account", authRoutes);
 app.use(userRoutes); 
 
 const sequelize = require("./data/db");
