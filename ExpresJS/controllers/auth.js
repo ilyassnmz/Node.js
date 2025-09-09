42a93daf5924bd14b724f50a82bba481deae644b
Ilyas Sonmez <lunap.ilyas@gmail.com>
2025-09-09 20:40:47 +0300

    Login page

--- ExpresJS/controllers/auth.js
+++ ExpresJS/controllers/auth.js
@@ -1,33 +1,80 @@
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
 
-exports.get_register = async function (req, res) {
-    try{
-        return res.render("auth/register",{
-            title: "Register"
+exports.get_register = async function(req, res) {
+    try {
+        return res.render("auth/register", {
+            title: "register"
         });
     }
-    catch(err){
+    catch(err) {
         console.log(err);
     }
 }
 
-exports.post_register = async function (req, res) {
+exports.post_register = async function(req, res) {
     const name = req.body.name;
     const email = req.body.email;
     const password = req.body.password;
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    try{
+    try {
         await User.create({
             fullname: name,
             email: email,
             password: hashedPassword
         });
+
         return res.redirect("login");
     }
-    catch(err){
+    catch(err) {
+        console.log(err);
+    }
+}
+
+exports.get_login = async function(req, res) {
+    try {
+        return res.render("auth/login", {
+            title: "login"
+        });
+    }
+    catch(err) {
+        console.log(err);
+    }
+}
+
+exports.post_login = async function(req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    try {
+
+        const user = await User.findOne({
+            where: {
+                email: email
+            }
+        });
+
+        if(!user) {
+            return res.render("auth/login", {
+                title: "login",
+                message: "Email hatalı"
+            });
+        }
+
+        const match = await bcrypt.compare(password, user.password);
+
+        if(match) {
+            return res.redirect("/");
+        } 
+        
+        return res.render("auth/login", {
+            title: "login",
+            message: "Parola hatalı"
+        });     
+    }
+    catch(err) {
         console.log(err);
     }
 }
