bee4d429f4860034b8066788783e257ecf067567
Ilyas Sonmez <lunap.ilyas@gmail.com>
2025-10-29 20:35:09 +0300

    Express Requests

--- shopapp/app.js
+++ shopapp/app.js
@@ -1,89 +1,19 @@
 const express = require("express");
-const { func } = require("joi");
 const app = express();
+const cors = require("cors");
 
-const Joi = require("joi");
+const products = require("./routes/products");
+const home = require("./routes/home");
 
 app.use(express.json());
 
-// http methods: get, post, put, delete
+app.use(cors({
+    origin: "*",
+    methods: ["GET"]
+}));
 
-const products = [
-    { id: 1, name: "iphone 12", price: 20000 },
-    { id: 2, name: "iphone 13", price: 30000 },
-    { id: 3, name: "iphone 14", price: 40000 }
-];
-
-app.get("/", (req, res) => {
-    res.send(products[0]);
-});
-
-app.get("/api/products", (req, res) => {
-    res.send(products);
-});
-
-app.post("/api/products", (req, res) => {
-    const { error } =  validateProduct(req.body);
-
-    if(error) {
-        return res.status(400).send(result.error.details[0].message);
-    }
-
-    const product = {
-        id: products.length + 1,
-        name: req.body.name,
-        price: req.body.price
-    };
-    products.push(product);
-    res.send(product);
-});
-
-app.put("/api/products/:id", (req, res) => {
-    const product = products.find(p => p.id == req.params.id);
-    if(!product) {
-        return res.status(404).send("aradığınız ürün bulunamadı.");
-    }
-
-    const { error } = validateProduct(req.body);
-
-    if(error) {
-        return res.status(400).send(result.error.details[0].message);
-    }
-
-    product.name = req.body.name;
-    product.price = req.body.price;
-
-    res.send(product);
-});
-
-app.delete("/api/products/:id", (req, res) => {
-    const product = products.find(p => p.id == req.params.id);
-    if(!product) {
-        return res.status(404).send("aradığınız ürün bulunamadı.");
-    }
-
-    const index = products.indexOf(product);
-    products.splice(index, 1);
-    res.send(product);
-});
-
-app.get("/api/products/:id", (req, res) => {
-    const product = products.find(p => p.id == req.params.id);
-
-    if(!product) {
-        return res.status(404).send("aradığınız ürün bulunamadı.");
-    }
-    res.send(product);
-});
-
-function validateProduct(product) {
-    const schema = new Joi.object({
-        name: Joi.string().min(3).max(30).required(),
-        price: Joi.number().required()
-    });
-
-    return schema.validate(product);
-}
+app.use("/api/products" ,products);
+app.use("/", home);
 
 
 app.listen(3000, () => {
